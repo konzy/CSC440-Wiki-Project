5c75a948f90c1946504d46afce26d09fed3ce3ab
Brian Konzman <bkonzman@gmail.com>
2017-10-30 17:49:12 -0400

    added to user test class

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,14 +1,111 @@
 from unittest import TestCase
-
-from wiki.web import user
-from wiki.web.routes import bp
-from wiki.web.user import UserManager, protect
-
+from wiki.web.user import UserManager
+import os
 
 class UserTestCase(TestCase):
     """
-        Contains various tests for the wiki.web.user.User class.
+        Tests creating a new json file, and tries to create a new file when one is already made
     """
+
+    def test_new_json_file(self):
+        os.remove("tests/users.json")
+        um = UserManager("tests")
+
+        um = UserManager("tests")
+
+    """
+        Tests creating a new json file, and tries to create a new file when one is already made
+    """
+    def test_user_read_write_json(self):
+        um = UserManager("tests")
+        json = um.read()
+
+        assert json is not None
+
+        um.write(json)
+
+        assert um.read() is not None
+
+    def test_user_creation_cleartext(self):
+        # print(os.getcwd())
+
+        username = "clear name"
+        password = "clear pass"
+        um = UserManager("tests")
+
+        cleartext_user = um.add_user(username, password, True, ["admin"], "cleartext")
+
+        assert cleartext_user is not None
+
+    def test_same_name(self):
+        username = "clear name"
+        password = "clear pass"
+        um = UserManager("tests")
+
+        cleartext_user = um.add_user(username, password, True, ["admin"], "cleartext")
+
+        assert cleartext_user is not None
+
+    def test_not_implemented_auth(self):
+        username = "none name"
+        password = "none pass"
+        um = UserManager("tests")
+
+        is_caught = False
+
+        try:
+            um.add_user(username, password, True, ["admin"], "asdf")
+        except NotImplementedError:
+            is_caught = True
+
+        assert is_caught is True
+
+    def test_user_creation_hash(self):
+        # print(os.getcwd())
+
+        username = "hash name"
+        password = "hash pass"
+        um = UserManager("tests")
+
+        hash_user = um.add_user(username, password, True, ["admin"], "hash")
+
+        assert hash_user is not None
+
+    def test_user_get(self):
+        username = "get name"
+        password = "get pass"
+        um = UserManager("tests")
+
+        cleartext_user = um.add_user(username, password, True, ["admin"], "cleartext")
+
+        assert um.get_user(username) is not None
+
+    def test_user_delete(self):
+        username = "delete name"
+        password = "delete pass"
+        um = UserManager("tests")
+
+        cleartext_user = um.add_user(username, password, True, ["admin"], "cleartext")
+
+        assert um.get_user(username) is not None
+
+        um.delete_user(username)
+
+        assert um.get_user(username) is None
+
+        assert um.delete_user("user never has been created") is False
+
+    def test_user_update(self):
+        username = "update name"
+        password = "update pass"
+        um = UserManager("tests")
+
+        cleartext_user = um.add_user(username, password, True, ["admin"], "cleartext")
+
+        data = cleartext_user.data
+
+        um.update(username, data)
+
 
     def test_simple_user(self):
         """
