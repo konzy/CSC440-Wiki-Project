3377d0b02b7e63f08c90249808fc34c8f9390a1d
ethan <gallaghere2@mymail.nku.edu>
2017-11-01 16:16:33 -0400

    Changed paths to relative

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 # encoding: utf-8
+import os
 
 SECRET_KEY='a unique and long key'
 TITLE='Megatron'
 HISTORY_SHOW_MAX=30
 PIC_BASE = '/static/content/'
-CONTENT_DIR = '/home/ethan/PycharmProjects/CSC440-Wiki-Project/content'
-USER_DIR = '/home/ethan/PycharmProjects/CSC440-Wiki-Project/user'
+CONTENT_DIR =  '/content'
+USER_DIR = '/user'
 NUMBER_OF_HISTORY = 5
 PRIVATE = True
