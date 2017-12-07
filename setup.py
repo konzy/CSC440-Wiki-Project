8c9ac47366b2d06a2191d5fd9e30f6c984a4a308
Brian Konzman <bkonzman@gmail.com>
2017-12-07 12:48:13 -0500

    PyPI additions

--- setup.py
+++ setup.py
@@ -36,5 +36,22 @@ setup(
     
     Create a wiki with user login, create, edit, delete, wiki pages.
     With features of google scholar lookup, download as pdf, email updated on page change.
-    """
+    """,
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ],
 )
